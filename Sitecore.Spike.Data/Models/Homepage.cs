b035201cfc0631869bebaee3d8bea978b48eca72
chrishey <chrishey87@hotmail.com>
2014-05-13 16:54:04 +0100

    Added layouts folder under views, added data project with homepage model

---
+++ Sitecore.Spike.Data/Models/Homepage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitecore.Spike.Data.Models
+{
+    public class HomePage
+    {
+        public virtual string Title { get; set; }
+        public virtual string Text { get; set; }
+    }
+}
